5e0165eebb3fffc8208e0009f0f7dce6294ebaab
William Kent <wjk011@gmail.com>
2020-03-24 20:12:35 -0400

    Reindent Package.swift

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "PTYKit",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(name: "PTYKit", targets: ["PTYKit"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "PTYKit", dependencies: ["CPTYFork"]),
+	name: "PTYKit",
+	products: [
+		// Products define the executables and libraries produced by a package, and make them visible to other packages.
+		.library(name: "PTYKit", targets: ["PTYKit"]),
+	],
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+		// .package(url: /* package url */, from: "1.0.0"),
+	],
+	targets: [
+		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+		// Targets can depend on other targets in this package, and on products in packages which this package depends on.
+		.target(name: "PTYKit", dependencies: ["CPTYFork"]),
 		.target(name: "CPTYFork", dependencies: []),
-        .testTarget(name: "PTYKitTests", dependencies: ["PTYKit"]),
-    ]
+		.testTarget(name: "PTYKitTests", dependencies: ["PTYKit"]),
+	]
 )
