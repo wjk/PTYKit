575c3ec33bce74f2c6f7dce8193f4a6dc4b1feb4
William Kent <wjk011@gmail.com>
2020-03-24 21:27:21 -0400

    Reindent

--- Sources/PTYKitSample/main.swift
+++ Sources/PTYKitSample/main.swift
@@ -14,9 +14,19 @@ do {
 	/// You can test this program with Xcode console.
 	///
 	let pty2 = PTY(processPath: "/bin/zsh", arguments: ["/bin/zsh"], environment: ["TERM=ansi"])!
+	var stopRunLoop = false
 	FileHandle.standardInput.readabilityHandler = { f in
 		let d = f.availableData
-		pty2.masterFileHandle.write(d)
+		if d.count > 0 {
+			pty2.masterFileHandle.write(d)
+		} else {
+			guard #available(macOS 10.15, *) else {
+				fatalError("can only run on macOS 10.15 or later")
+			}
+
+			try! pty2.masterFileHandle.close()
+			stopRunLoop = true
+		}
 	}
 	pty2.masterFileHandle.readabilityHandler = { f in
 		let d = f.availableData
@@ -24,10 +34,14 @@ do {
 			print("end of file, exiting now")
 			exit(0)
 		}
-		
+
 		let s = String(data: d, encoding: .utf8)!
 		print(s)
 	}
-	RunLoop.main.run()
+
+	while !stopRunLoop {
+		RunLoop.main.run(mode: .default, before: Date.distantPast)
+	}
+
 	pty2.waitForChildProcessExit()
 }
