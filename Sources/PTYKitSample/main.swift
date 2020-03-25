9b11e0e468a1f2049d5a1a60b2b630a18f1b6fc5
William Kent <wjk011@gmail.com>
2020-03-24 20:23:19 -0400

    Reindent PTYKitSample

--- Sources/PTYKitSample/main.swift
+++ Sources/PTYKitSample/main.swift
@@ -2,32 +2,32 @@ import Foundation
 import PTYKit
 
 do {
-    /// Basic usage.
-    let p = PTY(processPath: "/bin/ls", arguments: ["/bin/ls", "-Gbla"], environment: ["TERM=ansi"])!
+	/// Basic usage.
+	let p = PTY(processPath: "/bin/ls", arguments: ["/bin/ls", "-Gbla"], environment: ["TERM=ansi"])!
 	print(String(data: p.masterFileHandle.readDataToEndOfFile(), encoding: .utf8) ?? "<null>")
-    p.waitForChildProcessExit()
+	p.waitForChildProcessExit()
 }
 
 do {
-    ///
-    /// This runs infinite REPL loop.
-    /// You can test this program with Xcode console.
-    ///
-    let pty2 = PTY(processPath: "/bin/zsh", arguments: ["/bin/zsh"], environment: ["TERM=ansi"])!
-    FileHandle.standardInput.readabilityHandler = { f in
-        let d = f.availableData
-        pty2.masterFileHandle.write(d)
-    }
-    pty2.masterFileHandle.readabilityHandler = { f in
-        let d = f.availableData
+	///
+	/// This runs infinite REPL loop.
+	/// You can test this program with Xcode console.
+	///
+	let pty2 = PTY(processPath: "/bin/zsh", arguments: ["/bin/zsh"], environment: ["TERM=ansi"])!
+	FileHandle.standardInput.readabilityHandler = { f in
+		let d = f.availableData
+		pty2.masterFileHandle.write(d)
+	}
+	pty2.masterFileHandle.readabilityHandler = { f in
+		let d = f.availableData
 		guard d.count > 0 else {
 			print("end of file, exiting now")
 			exit(0)
 		}
-
-        let s = String(data: d, encoding: .utf8)!
-        print(s)
-    }
-    RunLoop.main.run()
-    pty2.waitForChildProcessExit()
+		
+		let s = String(data: d, encoding: .utf8)!
+		print(s)
+	}
+	RunLoop.main.run()
+	pty2.waitForChildProcessExit()
 }
